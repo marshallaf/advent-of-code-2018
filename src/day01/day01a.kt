a0fe08118be2899ce0cbc10f57fbff75e25464b9
Andrew Marshall <marshall.a.f@gmail.com>
2018-12-07 21:53:19 -0600

    Set up Kotlin project

---
+++ src/day01/day01a.kt
@@ -0,0 +1,12 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    val reader = File("./src/day01/day01a-input.txt").bufferedReader()
+    var sum = 0
+    reader.useLines { lines ->
+        lines.forEach {
+            sum += it.toInt()
+        }
+    }
+    println(sum)
+}
